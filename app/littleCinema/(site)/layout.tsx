a47abe2656369d5c6b80efff553445dbdbc7aad5
miraz-mondal <mirazmondal.official@gmail.com>
2023-07-21 16:15:01 +0530

    Little Cinema desc changed

--- app/littleCinema/(site)/layout.tsx
+++ app/littleCinema/(site)/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
             href="/"
             className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold"
           >
-            Kapehe
+            Little Cinema
           </Link>
           <div className="flex items-center gap-5 text-sm text-gray-600">
             {pages.map((page) => (
