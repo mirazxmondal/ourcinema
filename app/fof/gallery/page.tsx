e686445c77742b10baf312568636fac2725cf66c
miraz-mondal <mirazmondal.official@gmail.com>
2023-07-22 00:49:20 +0530

    FOF navbar and routes created

--- app/fof/gallery/page.tsx
+++ app/fof/gallery/page.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 function page() {
   return (
     <div>
-      <div>fof home</div>
+        <div>
+            gallery
+        </div>
     </div>
   )
 }
--- app/fof/schedule/page.tsx
+++ app/fof/schedule/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div>
-      <div>fof home</div>
+        <div>schedule</div>
     </div>
   )
 }
