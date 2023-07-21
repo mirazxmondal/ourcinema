d69ea416835e0a12e34234e36e0680a54e8e1794
miraz-mondal <mirazmondal.official@gmail.com>
2023-07-22 00:30:37 +0530

    Navbar structure done

--- app/components/MainNavbar.tsx
+++ app/components/MainNavbar.tsx
@@ -5,12 +5,54 @@ import "../globals.css";
 function MainNavbar() {
   return (
     <div>
-      <div className="flex justify-between space-x-4">
-        <Link href="/">HOME</Link>
-        <Link href="/noticeboard">NOTICE BOARD</Link>
-        <Link href="/inthenews">IN THE NEWS</Link>
-        <Link href="/publications">PUBLICATIONS</Link>
-        <Link href="/supportus">SUPPORT US</Link>
+      <div className="h-16 mx-auto px-32 flex items-center justify-between font-semibold">
+        <Link href="/" className="text-2xl cursor-pointer">
+          Logo
+        </Link>
+        <ul className="flex items-center gap-5">
+          <li>
+            {" "}
+            <Link href="/" className="hover:text-red-500 transition-colors">
+              HOME
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link
+              href="/noticeboard"
+              className="hover:text-red-500 transition-colors"
+            >
+              NOTICE BOARD
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link
+              href="/inthenews"
+              className="hover:text-red-500 transition-colors"
+            >
+              IN THE NEWS
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link
+              href="/publications"
+              className="hover:text-red-500 transition-colors"
+            >
+              PUBLICATIONS
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link
+              href="/supportus"
+              className="hover:text-red-500 transition-colors"
+            >
+              SUPPORT US
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
