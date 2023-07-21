fddac87ef5a1723ef547a55d839dd2c389351656
miraz-mondal <mirazmondal.official@gmail.com>
2023-07-22 00:09:37 +0530

    Footer done

--- app/components/MainFooter.tsx
+++ app/components/MainFooter.tsx
@@ -4,15 +4,13 @@ import "../globals.css";
 
 function MainFooter() {
   return (
-    <div className=" bg-black text-white pt-10 pb-8 pr-10 pl-10">
+    <div className=" bg-black text-white pt-10 pb-8 pr-24 pl-24">
       <div className=" flex flex-row">
         {/* GET IN TOUCH */}
         <div className="pr-52">
-          <p className="text-3xl font-medium">GET IN TOUCH</p>
-          <form action="" className="text-xl flex flex-col">
-            <label htmlFor="" className="">
-              Name
-            </label>
+          <p className="text-3xl font-medium mb-6">GET IN TOUCH</p>
+          <form action="" className="text-xl flex flex-col w-80 space-y-2">
+            <label htmlFor="">Name</label>
             <input type="text" />
             <label htmlFor="">Email</label>
             <input type="email" />
@@ -20,19 +18,23 @@ function MainFooter() {
             <input type="text" />
             <label htmlFor="">Message</label>
             <input type="text" className="h-52" />
+            <input
+              type="submit"
+              className="bg-white text-black mt-8 font-semibold w-28 cursor-pointer"
+            />
           </form>
         </div>
         <p className="line pr-32"></p>
         {/* CONTACT US */}
-        <div className="text-xl flex flex-col space-y-3">
+        <div className="text-xl flex flex-col space-y-3 pl-24 pt-16 tracking-widest">
           <div>
-          <p>CONTACT US</p>
-          <p>EMAIL - abc@gmail.com</p>
-          <p>PHONE - 700000000</p>
+            <p>CONTACT US</p>
+            <p>EMAIL - abc@gmail.com</p>
+            <p>PHONE - 700000000</p>
           </div>
           <div>
-          <p>CONTACT WITH US</p>
-          <div>icons</div>
+            <p>CONTACT WITH US</p>
+            <div>icons</div>
           </div>
           <div>
             <div>
