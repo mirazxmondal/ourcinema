4dfaf3777c7cac8e72179cc54ea519b276252aaf
miraz-mondal <mirazmondal.official@gmail.com>
2023-07-23 00:17:14 +0530

    fof landing page WIP

--- app/fof/page.tsx
+++ app/fof/page.tsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import FOFFooter from '../components/FOFFooter'
-import Location from '../components/Location'
+import React from 'react';
+import FOFFooter from '../components/FOFFooter';
+import Location from '../components/Location';
+import fof from '../../Database/fof.json';
+import Link from 'next/link';
 
-function page() {
+interface FOFData {
+  image: string;
+}
+
+function page(): JSX.Element {
   return (
     <div>
-      <div>fof home</div>
+      <div>
+        <button>2023</button>
+        {fof[2023].map((data: FOFData) => (
+          <Link href={data.image} key={data.image}>
+            <div className='border-2 border-sky-500 h-[400px] w-[400px]'>
+              {
+                
+              }
+            </div>
+          </Link>
+        ))}
+      </div>
       <Location />
       <FOFFooter />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
