1b323104fbd3cd2cc5f60505c96123bd373bd031
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-07-23 00:02:09 +0530

    stories schema added

---
+++ sanity/schemas/story-schema.ts
@@ -0,0 +1,77 @@
+const story = {
+  name: "stories",
+  title: "stories",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "name" },
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: { hotspot: true },
+      fields: [
+        {
+          name: "alt",
+          title: "Alt",
+          type: "string",
+        },
+      ],
+    },
+    {
+      name: "url",
+      title: "URL",
+      type: "url",
+    },
+    {
+      name: "content",
+      title: "Content",
+      type: "array",
+      of: [
+        {
+          type: "block",
+          styles: [
+            { title: "Normal", value: "normal" },
+            { title: "H1", value: "h1" },
+            { title: "H2", value: "h2" },
+            { title: "H3", value: "h3" },
+            { title: "Quote", value: "blockquote" },
+          ],
+          marks: {
+            decorators: [{ title: "Strong", value: "strong" }],
+            annotations: [
+              {
+                name: "image",
+                type: "image",
+                title: "Image",
+                fields: [
+                  {
+                    name: "caption",
+                    type: "string",
+                    title: "Caption",
+                  },
+                  {
+                    name: "alt",
+                    type: "string",
+                    title: "Alt Text",
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      ],
+    },
+  ],
+};
+
+export default story;
---
+++ sanity/schemas/story-section-schema.ts
@@ -0,0 +1,59 @@
+const stories = {
+  name: "stories-page",
+  title: "Stories-page",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "title", maxLength: 96 },
+    },
+    {
+      name: "content",
+      title: "Content",
+      type: "array",
+      of: [
+        {
+          type: "block",
+          styles: [
+            { title: "Normal", value: "normal" },
+            { title: "H1", value: "h1" },
+            { title: "H2", value: "h2" },
+            { title: "H3", value: "h3" },
+            { title: "Quote", value: "blockquote" },
+          ],
+          marks: {
+            decorators: [{ title: "Strong", value: "strong" }],
+            annotations: [
+              {
+                name: "image",
+                type: "image",
+                title: "Image",
+                fields: [
+                  {
+                    name: "caption",
+                    type: "string",
+                    title: "Caption",
+                  },
+                  {
+                    name: "alt",
+                    type: "string",
+                    title: "Alt Text",
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      ],
+    },
+  ],
+};
+
+export default stories;
