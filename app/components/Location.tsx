9564573f6809fbc5642d8f30e58f08e80f92abd8
miraz-mondal <mirazmondal.official@gmail.com>
2023-07-22 21:58:55 +0530

    location components alignment fixed

--- app/components/Location.tsx
+++ app/components/Location.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Location() {
   return (
     <div>
-    <div className="bg-[#d9d9df] flex flex-row mr-48 ml-48 p-10 mb-10 rounded-md">
+    <div className="bg-[#d9d9df] flex flex-row mr-48 ml-48 p-10 mb-10 mt-10 rounded-md">
       {/* GET IN TOUCH */}
       <div>
       <div className="pr-56">
@@ -27,7 +27,7 @@ function Location() {
       {/* FESTIVAL LOCATION */}
       <div>
         <p className="text-3xl font-medium mb-6">FESTIVAL LOCATION</p>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14742.064866442734!2d88.3487472!3d22.5223274!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a027731576b6be7%3A0x96a67c9579d27e65!2sMahanayak%20Uttam%20Manch!5e0!3m2!1sen!2sin!4v1689969223548!5m2!1sen!2sin" className="w-[500px] h-[473px]" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14742.064866442734!2d88.3487472!3d22.5223274!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a027731576b6be7%3A0x96a67c9579d27e65!2sMahanayak%20Uttam%20Manch!5e0!3m2!1sen!2sin!4v1689969223548!5m2!1sen!2sin" className="w-[500px] h-[495px]" loading="lazy"></iframe>
       </div>
     </div>
     </div>
