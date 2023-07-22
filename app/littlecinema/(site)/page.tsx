f6347d50a00cc1bd9d6d1940b4b7085173916c0f
miraz-mondal <mirazmondal.official@gmail.com>
2023-07-23 00:47:55 +0530

    LC RELATED ARTICLES cards aligned

--- app/littlecinema/(site)/page.tsx
+++ app/littlecinema/(site)/page.tsx
@@ -9,26 +9,16 @@ export default async function Home() {
   return (
     <div>
       <LittleCinemaBanner />
-      {/* <h1 className="text-7xl font-extrabold">
-        Hello I&apos;m
-        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
-          {" "}
-          Mr.Bot!
-        </span>
-      </h1>
-      <p className="mt-3 text-xl text-gray-600">
-        Aloha everyone! Check out my projects!
-      </p> */}
       <div className="flex flex-row space-x-2">
         <h2 className="font-bold text-3xl">RELATED</h2>
         <h2 className="font-bold text-red-600 text-3xl">ARTICLES</h2>
       </div>
       <div className="flex flex-wrap justify-center">
-        <div className="mt-5 flex flex-col justify-center items-center w-[620px]">
-          <div className="max-w-full flex flex-row space-x-5 ">
+        <div className="mt-5 flex flex-col justify-center items-center">
+          <div className="max-w-full flex flex-row space-x-5">
             <Link href={`LittleCinema/projects/${projects[0].slug}`}>
               <div
-                className="h-[342px] w-[400px] border border-black rounded-2xl"
+                className="h-[500px] w-[500px] border border-black rounded-2xl"
                 style={{
                   backgroundImage: `url(${projects[0].image})`,
                   backgroundSize: "cover",
@@ -43,7 +33,7 @@ export default async function Home() {
 
             <Link href={`LittleCinema/projects/${projects[4].slug}`}>
               <div
-                className="h-[342px] w-[200px] border border-black rounded-2xl"
+                className="h-[500px] w-[300px] border border-black rounded-2xl"
                 style={{
                   backgroundImage: `url(${projects[4].image})`,
                   backgroundSize: "cover",
@@ -55,7 +45,7 @@ export default async function Home() {
 
           <Link href={`LittleCinema/projects/${projects[2].slug}`}>
             <div
-              className="w-[620px] h-52 border border-black mt-5 rounded-2xl"
+              className="w-[825px] h-[350px] border border-black mt-5 rounded-2xl"
               style={{
                 backgroundImage: `url(${projects[2].image})`,
                 backgroundSize: "cover",
@@ -68,7 +58,7 @@ export default async function Home() {
           <div className="mt-5 flex space-x-5">
             <Link href={`LittleCinema/projects/${projects[3].slug}`}>
               <div
-                className="h-[354px] w-[240px] rounded-2xl border border-black"
+                className="h-[510px] w-[300px] rounded-2xl border border-black"
                 style={{
                   backgroundImage: `url(${projects[3].image})`,
                   backgroundSize: "cover",
@@ -80,7 +70,7 @@ export default async function Home() {
             <div className="space-y-3 flex flex-col">
               <Link href={`LittleCinema/projects/${projects[1].slug}`}>
                 <div
-                  className="h-[171px] w-[360px] border rounded-2xl border-black"
+                  className="h-[250px] w-[500px] border rounded-2xl border-black"
                   style={{
                     backgroundImage: `url(${projects[1].image})`,
                     backgroundSize: "cover",
@@ -90,7 +80,7 @@ export default async function Home() {
               </Link>
               <Link href={`LittleCinema/projects/${projects[5].slug}`}>
                 <div
-                  className="h-[171px] w-[360px] rounded-2xl border border-black"
+                  className="h-[250px] w-[500px] rounded-2xl border border-black"
                   style={{
                     backgroundImage: `url(${projects[5].image})`,
                     backgroundSize: "cover",
--- app/littlecinema/card/CardComponent.jsx
+++ app/littlecinema/card/CardComponent.jsx
@@ -8,7 +8,9 @@ const CardComponent = () => {
         <div className="h-[342px] w-[200px] border border-black rounded-2xl"></div>
       </div>
       <div className="w-[620px] h-52 border border-black mt-5 rounded-2xl">
-        <div></div>
+        <div>
+          
+        </div>
       </div>
       <div className="mt-5 flex space-x-5">
         <div className="h-[354px] w-[240px] rounded-2xl border border-black"></div>
